cd7677f07687a5ae62598728a430283ee84d9c50
Mckelle Gravelle <GravelleMckelle@gmail.com>
2022-05-20 04:35:17 -0500

    Add documentation to rerender types

--- src/main/java/ravioli/gravioli/stategui/gui/partition/state/RerenderType.java
+++ src/main/java/ravioli/gravioli/stategui/gui/partition/state/RerenderType.java
@@ -1,6 +1,13 @@
 package ravioli.gravioli.stategui.gui.partition.state;
 
 public enum RerenderType {
+    /**
+     * Menus will always re-render when a state change occurs.
+     */
     ALWAYS,
+    /**
+     * Menus will only re-render when a state change occurs IFF the state change would result
+     * in a visible change in the menu.
+     */
     ONLY_ON_RENDER_CHANGE;
 }
